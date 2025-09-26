b249be7d815a5804adcc7422589eaac5e4337187
Mr47-James <benlouazzani.hamza@gmail.com>
2025-09-26 20:20:26 +0100

    1st main commit

---
+++ index.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+
+db = SQLAlchemy()
+
+def init_db(app: Flask):
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://user:password@localhost/musicdb"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+    return db
